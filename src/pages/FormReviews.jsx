e8c3523eb5fd0a4db681e4bf887df4f6cb007e5a
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-28 16:33:08 +0100

    fine form

--- src/pages/FormReviews.jsx
+++ src/pages/FormReviews.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-function ReviewForm() {
+function ReviewForm({ fetchMovie }) {
+    const { id } = useParams();
     const [name, setName] = useState('');
     const [text, setText] = useState('');
     const [vote, setVote] = useState('');
-    const [date, setDate] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -13,73 +15,62 @@ function ReviewForm() {
             name,
             text,
             vote,
-            date,
         };
 
-        console.log(reviewData);
-
-        setName('');
-        setText('');
-        setVote(1);
-        setDate('');
+        axios.post(`http://localhost:3000/movies/${id}/reviews`, reviewData)
+            .then(() => {
+                fetchMovie();
+                setName('');
+                setText('');
+                setVote(1);
+            })
+            .catch((error) => {
+                console.error("Errore durante l'invio della recensione:", error);
+            });
     };
 
     return (
-        <div className="mb-4">
-            <h4 className='d-flex justify-content-center'style={{ color: 'black' }}> <strong>Aggiungi la tua recensione</strong></h4>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label my-2" style={{ color: 'black' }}> <strong>Nome</strong></label>
-                    <input
-                        type="text"
-                        id="name"
-                        className="form-control"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="text" className="form-label" style={{ color: 'black' }}> <strong>Recensione</strong></label>
-                    <textarea
-                        id="text"
-                        className="form-control"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="date" className="form-label" style={{ color: 'black' }}> <strong>Data di Pubblicazione</strong></label>
-                    <input
-                        type="date"
-                        id="date"
-                        className="form-control"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="vote" className="form-label" style={{ color: 'black' }}> <strong>Voto (1-5)</strong></label>
-                    <input
-                        type="number"
-                        id="vote"
-                        className="form-control"
-                        value={vote}
-                        onChange={(e) => setVote(e.target.value)}
-                        min="1"
-                        max="5"
-                        required
-                    />
-                </div>
-                <div className='d-flex justify-content-center p-2 pb-5'>
-                    <button type="submit" className="btn btn-primary">
-                        Invia Recensione
-                    </button>
-                </div>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label my-2" style={{ color: 'black' }}> <strong>Nome</strong></label>
+                <input
+                    type="text"
+                    id="name"
+                    className="form-control"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label" style={{ color: 'black' }}> <strong>Recensione</strong></label>
+                <textarea
+                    id="text"
+                    className="form-control"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    required
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="vote" className="form-label" style={{ color: 'black' }}> <strong>Voto (1-5)</strong></label>
+                <input
+                    type="number"
+                    id="vote"
+                    className="form-control"
+                    value={vote}
+                    onChange={(e) => setVote(e.target.value)}
+                    min="1"
+                    max="5"
+                    required
+                />
+            </div>
+            <div className="d-flex justify-content-center p-2 pb-5">
+                <button type="submit" className="btn btn-primary">
+                    Invia Recensione
+                </button>
+            </div>
+        </form>
     );
 }
 
--- src/pages/MoviesDetails.jsx
+++ src/pages/MoviesDetails.jsx
@@ -1,24 +1,30 @@
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import FormReviews from './FormReviews';
+
 function MovieDetail() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
+    const fetchMovie = () => {
+        setLoading(true);
         axios.get(`http://localhost:3000/movies/${id}`)
-            .then(response => {
+            .then((response) => {
                 setMovie(response.data.data);
                 setLoading(false);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Errore nella richiesta:', error);
                 setError('Film non trovato');
                 setLoading(false);
             });
+    };
+
+    useEffect(() => {
+        fetchMovie(); 
     }, [id]);
 
     if (loading) {
@@ -34,12 +40,17 @@ function MovieDetail() {
             <h1 className="text-center mb-4">{movie.title}</h1>
             <div className="row ">
                 <div className="col-md-6 d-flex justify-content-center">
-                    <img className="movie-image rounded-lg" src={`/${movie.image}`} alt={movie.title} style={{ width: 'auto', height: '500px' }} />
+                    <img
+                        className="movie-image rounded-lg"
+                        src={`/${movie.image}`}
+                        alt={movie.title}
+                        style={{ width: 'auto', height: '500px' }}
+                    />
                 </div>
                 <div className="col-md-6">
                     <h3 style={{ color: 'black' }}><strong>Recensione Film :</strong></h3>
                     <p>{movie.abstract}</p>
-                    <h3 className="mt-4 mb-4"style={{ color: 'black' }}><strong>Recensione Utenti :</strong></h3>
+                    <h3 className="mt-4 mb-4" style={{ color: 'black' }}><strong>Recensione Utenti :</strong></h3>
                     <div className="list-group">
                         {movie.reviews.length > 0 ? (
                             movie.reviews.map((review) => (
@@ -48,7 +59,6 @@ function MovieDetail() {
                                     <h6><strong>Data di Pubblicazione : </strong>{review.created_at}</h6>
                                     <h6><strong>Voto : </strong>{review.vote}</h6>
                                 </div>
-
                             ))
                         ) : (
                             <p>Non ci sono recensioni per questo film.</p>
@@ -57,8 +67,8 @@ function MovieDetail() {
                 </div>
             </div>
             <div>
-                <div className='m-5'>
-                    <FormReviews />
+                <div className="m-5">                    
+                    <FormReviews fetchMovie={fetchMovie} />
                 </div>
             </div>
         </div>
